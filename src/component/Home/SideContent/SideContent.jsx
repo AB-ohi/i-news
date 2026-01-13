ba24e3e11e9bcf8382f8e9e0e50c6bb513315cd3
AB-Ohi <askohi63907@gmail.com>
2026-01-13 23:53:29 +0600

    update the side card

--- src/component/Home/SideContent/SideContent.jsx
+++ src/component/Home/SideContent/SideContent.jsx
@@ -1,34 +1,43 @@
 "use client";
 import React, { useState } from "react";
 import "./SideContent.css";
-
-const SideContent = () => {
-  const newsArray = [
+ const newsArray = [
     {
-      img: "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/612100380_122104887729192244_2806682581351881423_n.jpg?stp=dst-jpg_s565x565_tt6&_nc_cat=101&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEfjRQDJ1XoRJD20UyNjmX88y1NnpZhsQbzLU2elmGxBhc4zr0Ty8zwHsZJivs3LvCMlCYHcw8DhWefjLmN6Ios&_nc_ohc=jvBVAStbeO8Q7kNvwH1HBeL&_nc_oc=AdkG4gj7p_Er2wBb_il9TN6HbkhqMYStZcuO0cVgCGxxDvpsuotXfAP0B1tKboqTToA&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=KZqjC-3Z29WS7EQcH4nkcQ&oh=00_AfqSU51cSIWMgzEbC-a7rR7RkqEYLrZ_TfAerqGNHKM7NA&oe=696C2941",
-      headline: "ঢাকায় নতুন মেট্রোরেল চালু",
-      description: "আজ থেকে ঢাকায় নতুন রুটে মেট্রোরেল চলাচল শুরু হয়েছে।",
+      img: "https://www.bvnews24.com/media/imgAll/2024April/SM/edu-2601131307-SM.png",
+      headline: "ঢাকায় নতুন রুটে আধুনিক মেট্রোরেল পরিষেবা আনুষ্ঠানিকভাবে চালু",
+      description:
+        "আজ থেকে ঢাকায় নতুন রুটে অত্যাধুনিক মেট্রোরেল পরিষেবা চালু করা হয়েছে। এর মাধ্যমে যাত্রীদের যাতায়াত আরও সহজ, দ্রুত ও নিরাপদ হবে বলে আশা করা হচ্ছে।",
     },
     {
       img: "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/614905881_122243797874089912_3183486502617996051_n.jpg?stp=dst-jpg_p526x296_tt6&_nc_cat=1&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeFlNmhhNmF-_MMvXgmzgYc_kcy-xx4ch4mRzL7HHhyHiVSIYfVFEXFcCJWURbINTxtXkuJOnkDTwAwKq6ikEk6_&_nc_ohc=2YuaxIEWjRQQ7kNvwHwXl4y&_nc_oc=AdnzA6jXiRRXabk007hcAVUqWuhYWQRppCFI5YyKrDrG1SrdxM7U0GkPtiHtAfObPC4&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=fd6JqBqnbrZLZiMtqKD_Jw&oh=00_AfrN3ruenX8FMd2q0O1dxwUlMmly9DUwOUmYcG-BBkY_hQ&oe=696C1430",
-      headline: "বাংলাদেশ জিতেছে",
-      description: "রোমাঞ্চকর ম্যাচে বাংলাদেশ দল জয় লাভ করেছে।",
+      headline:
+        "রোমাঞ্চকর ম্যাচে শক্তিশালী প্রতিপক্ষকে হারিয়ে বাংলাদেশের দারুণ জয়",
+      description:
+        "উত্তেজনাপূর্ণ ও হাড্ডাহাড্ডি লড়াইয়ের পর শক্তিশালী প্রতিপক্ষকে পরাজিত করে বাংলাদেশ দল গুরুত্বপূর্ণ জয় অর্জন করেছে, যা সমর্থকদের মধ্যে আনন্দের জোয়ার বইয়ে দিয়েছে।",
     },
     {
       img: "https://scontent.fdac7-1.fna.fbcdn.net/v/t39.30808-6/613914874_122243733314089912_6288390776718212757_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeG51bTfLGtbPeFH2b5uxogl8txQhXDj2MTy3FCFcOPYxD80GBF-CtTek1tPPHqGAeQjJyLsYu8J_nblDOnHqDRb&_nc_ohc=mks5xjHADHoQ7kNvwHhv8wW&_nc_oc=Adl5FVvlEwDDEZMOFIT--vmrMWTAffQ3fABdtFxJVpKZ8R1vyoTK-WQueRKgv9nyE_g&_nc_zt=23&_nc_ht=scontent.fdac7-1.fna&_nc_gid=7aQ8iXdcbwUy6HMNh0_8pQ&oh=00_AfoChfmmN7bjuOaGLYUjvlnMmxU9yy2QDZ2PJ-wGgX04dg&oe=696C1C60",
-      headline: "আবহাওয়ার সতর্কতা",
-      description: "আগামী ২৪ ঘণ্টায় ভারী বৃষ্টির সম্ভাবনা রয়েছে।",
+      headline:
+        "দেশজুড়ে ভারী বৃষ্টিপাতের সম্ভাবনায় আবহাওয়া অধিদপ্তরের সতর্কতা জারি",
+      description:
+        "আবহাওয়া অধিদপ্তর জানিয়েছে, আগামী ২৪ ঘণ্টায় দেশের বিভিন্ন অঞ্চলে ভারী থেকে অতিভারী বৃষ্টিপাত হতে পারে। এ কারণে সংশ্লিষ্ট সবাইকে সতর্ক থাকার পরামর্শ দেওয়া হয়েছে।",
     },
   ];
-  //   console.log(newsArray);
+const SideContent = () => {
+ const lineSlice = (text,wordLimit = 7)=>{
+    const words = text.split(' ');
+    if(words.length >= wordLimit){
+        return words.slice(0,wordLimit).join(' ')+'...more'
+    }
+ }
   return (
     <div className="sideContentMain">
       {newsArray.map((news, index) => {
         return (
           <div className="sideCard" key={index}>
             <img className="sideImg" src={news.img} alt="" />
-            <div>
-              <h1>{news.headline}</h1>
+            <div className="newsCardDetails">
+              <h1>{lineSlice(news.headline, 7)}</h1>
               {/* <p>{news.description}</p> */}
             </div>
           </div>
