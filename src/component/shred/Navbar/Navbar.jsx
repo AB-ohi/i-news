4bea07f97544af103a9ea8803859e6db781ccb9a
AB-Ohi <askohi63907@gmail.com>
2026-03-28 02:09:39 +0600

    added

--- src/component/shred/Navbar/Navbar.jsx
+++ src/component/shred/Navbar/Navbar.jsx
@@ -242,11 +242,9 @@ const Navbar = () => {
                 placeholder="Search..."
                 onFocus={() => setIsFocused(true)}
                 onBlur={() => setIsFocused(false)}
-                className="
-                  flex bg-transparent border-none outline-none 
+                className="flex bg-transparent border-none outline-none 
                   text-white placeholder-white/50
-                  text-base w-full
-                "
+                  text-base w-full"
               />
             </div>
 
